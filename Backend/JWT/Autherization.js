53b1101df8a30d4179d6e77c147e4dd7584b6456
iPoojatanwar <tanwarpooja324@gmail.com>
2025-08-05 12:53:30 +0530

    Modify Autherization.js

--- Backend/JWT/Autherization.js
+++ Backend/JWT/Autherization.js
@@ -1,15 +1,13 @@
-import jwt from 'jsonwebtoken';
-import User from '../Model/userModel.js';
-export const jwtCookieToken= (userId , res)=>{
-    const token= jwt.sign({userId},process.env.JWT_TOKEN ,{
-        expiresIn:"1d"
-    })
-    res.cookie("jwt",token,{
-httpOnly:true,
-secure:true,
-sameSite:"none",
-    })
-    return token
+export const jwtCookieToken = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_TOKEN, {
+    expiresIn: "1d",
+  });
 
- }
- 
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production", 
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", 
+  });
+
+  return token;
+};
